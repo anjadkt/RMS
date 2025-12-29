6c89d410195dbb58171c121c9d19210c6ab56e64
anjadkt <anjad076@gmail.com>
2025-12-29 10:33:00 +0530

    add order history

---
+++ frontend/src/components/Order.jsx
@@ -0,0 +1,87 @@
+export default function OrderHistory({ data }) {
+  const total = data.orderItems.reduce(
+    (accum, val) => accum + val.subTotal,
+    0
+  );
+
+  return (
+    <>
+      <div
+        key={data._id}
+        className="bg-white w-[340px] border border-[#cd0045]  rounded-lg shadow-md mb-8 relative"
+      >
+        <div className="
+        px-2 py-0 rounded-2xl 
+        absolute -top-2 left-1/2 -translate-x-14 
+        bg-gradient-to-b from-[#fff5f7] to-white">
+          <div className="text-xs font-semibold text-[#cd0045] tracking-wide">
+            {data.orderId}
+          </div>
+        </div>
+
+        <div className="px-4 py-2 text-xs text-gray-600 flex justify-between mt-2 border-b border-gray-300">
+          <div>
+            <span className="font-semibold text-gray-700">Table Number :</span>{" "}
+            {data.tableNumber}
+          </div>
+          <div>
+            <span className="font-semibold text-gray-700">Order Type :</span>{" "}
+            {data.orderType}
+          </div>
+        </div>
+
+        <div className="divide-y divide-gray-100">
+          {data.orderItems?.map((v, i) => (
+            <div
+              key={v.itemId}
+              className="flex items-center justify-between px-4 py-2"
+            >
+              <div className="bg-gray-100 p-2 rounded-md w-[50px] flex items-center justify-center">
+                <img
+                  className="h-8 object-contain"
+                  src={v.image}
+                  alt={v.name}
+                />
+              </div>
+
+              <div className="flex items-center gap-2 font-[REM] text-xs text-gray-600">
+                <span className="font-medium">{v.name}</span>
+                <span className="text-gray-500">x</span>
+                <span className="font-medium">{v.quantity}</span>
+              </div>
+
+              <div className="text-xs font-medium text-gray-700">
+                ₹{v.subTotal}
+              </div>
+            </div>
+          ))}
+
+          <div className="flex justify-between px-5 py-2 bg-gray-50">
+            <span className="text-sm font-semibold text-gray-700">
+              Total
+            </span>
+            <span className="text-sm font-bold text-gray-900">
+              ₹{total}
+            </span>
+          </div>
+        </div>
+
+        <div className="flex font-[REM] justify-center px-4 py-2 border-t border-gray-100">
+          <span
+            className={`text-sm font-semibold px-10 py-0.5 rounded-lg
+              ${
+                data.status === "completed"
+                  ? "bg-green-100 text-green-700"
+                  : data.status === "pending"
+                  ? "bg-yellow-100 text-yellow-700"
+                  : "bg-gray-100 text-gray-600"
+              }
+            `}
+          >
+            {data.status}
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -1,7 +1,64 @@
+import {useNavigate} from "react-router-dom"
+import {useState,useEffect} from 'react'
+import api from "../services/axios";
+import DotLoader from "../components/DotLoader";
+import OrderHistory from "../components/Order.jsx"
+
 export default function History(){
+  const [orders,setOrders] = useState([]);
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    async function fetchOrders() {
+      try{
+        setLoading(true);
+        const {data} = await api.get('/user/order');
+        setOrders(data.orders);
+      }catch(error){
+        console.log(error.message);
+      }finally{
+        setLoading(false);
+      }
+    }
+    fetchOrders()
+  },[])
+
+  if(loading)return <DotLoader/>
   return (
     <>
-     History
+     <div className="bg-[#fff5f7] p-4 pt-2 ">
+        <div className="flex items-center justify-between -ml-2 mb-2">
+          <button onClick={()=>navigate('/')}>
+            <img className="h-3" src="/icons/leftArrow.png" alt="" />
+          </button>
+          <h1 className="text-lg font-semibold text-gray-800">History</h1>
+          <div></div>
+        </div>
+     </div>
+     <main className="px-5 flex flex-wrap justify-between">
+      {
+        orders.length < 1 ? (
+          <div className="flex flex-col items-center justify-center text-center py-16">
+            <h1 className="text-xl font-bold text-gray-800 mb-2">
+              No Orders Yet! 🍽️
+            </h1>
+
+            <p className="text-sm text-gray-500 max-w-xs">
+              Looks like your plate is still empty.  
+              Explore the menu and place your first order — delicious food awaits! 🍔🍕
+            </p>
+            <button className="bg-[#cd0045] mt-2 px-2 text-white text-sm font-semibold py-1 rounded-sm" onClick={()=>navigate('/')}>
+              View Menu
+            </button>
+          </div>
+        ) : (
+            orders?.map(v =>(
+            <OrderHistory data={v} />
+          ))
+        )
+      }
+     </main>
     </>
   )
 }
